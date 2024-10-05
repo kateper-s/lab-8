8265a01272ff951b22c2a1ef64d12ef5c3583536
Ponomareva Ekaterina <katya.ponomar006@mail.ru>
2024-10-05 10:42:27 +0300

    обновление

--- time.pas
+++ time.pas
@@ -1,5 +1,11 @@
 ﻿##
-var year:= readint64('Введите год:');
-if (year mod 4 = 0) and (year mod 100 <> 0) and (year mod 400 = 0) then
-  print('Високосный')
-else print('Не високосный');
+var year := readint64('Введите год:');
+if (year mod 4 = 0) then
+  if (year mod 100 = 0) and (year mod 400 <> 0) then
+    year := 365
+  else year := 366
+else year := 365;
+println($'Количество дней в году:{year}');
+
+if year = 365 then println('Невисокосный год')
+else println('Високосный год')
---
+++ timer.pas
@@ -0,0 +1,5 @@
+﻿##
+var timerh:= readint64('Введите час:');
+var timerm:= readint64('Введите минуты:');
+var timers:= readint64('Введите секунды:');
+print(timerh*3600+timerm*60+timers);
